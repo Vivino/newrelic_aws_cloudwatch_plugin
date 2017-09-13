2a4c32310e2ee22ba18ec02ac699637358f105f1
Oleksandr Vilchynskyy <oleksandr.vilchynskyy@vivino.com>
2017-09-13 18:53:01 +0300

    ALB beautification.

--- lib/newrelic_aws/collectors/alb.rb
+++ lib/newrelic_aws/collectors/alb.rb
@@ -3,33 +3,33 @@ module NewRelicAWS
     class ALB < Base
       def load_balancers
         alb = Aws::ElasticLoadBalancingV2::Client.new(
-          :access_key_id => @aws_access_key,
-          :secret_access_key => @aws_secret_key,
-          :region => @aws_region
+          access_key_id: @aws_access_key,
+          secret_access_key: @aws_secret_key,
+          region: @aws_region
         )
-        alb.describe_load_balancers.load_balancers.map { |load_balancer| load_balancer.load_balancer_arn.split(/:/)[-1].gsub("loadbalancer/", "") }
+        alb.describe_load_balancers.load_balancers.map { |load_balancer| load_balancer.load_balancer_arn.split(/:/)[-1].gsub('loadbalancer/', '') }
       end
 
       def metric_list
         [
-          ["ActiveConnectionCount",           "Average"   "Count", 0],
-          ["ClientTLSNegotiationErrorCount",  "Sum",      "Count", 0],
-          ["ConsumedLBCapacityUnits",         "Sum",      "Count", 0],
-          ["HealthyHostCount",                "Maximum"   "Count", 0],
-          ["HTTPCode_ELB_4XX",                "Sum",      "Count", 0],
-          ["HTTPCode_ELB_5XX",                "Sum",      "Count", 0],
-          ["HTTPCode_Target_2XX_Count",       "Sum",      "Count", 0],
-          ["HTTPCode_Target_3XX_Count",       "Sum",      "Count", 0],
-          ["HTTPCode_Target_4XX_Count",       "Sum",      "Count", 0],
-          ["HTTPCode_Target_5XX_Count",       "Sum",      "Count", 0],
-          ["NewConnectionCount",              "Sum",      "Count", 0],
-          ["ProcessedBytes",                  "Average"   "Bytes", 0],
-          ["RequestCount",                    "Sum",      "Count", 0],
-          ["RejectedConnectionCount",         "Sum",      "Count", 0],
-          ["TargetConnectionErrorCount",      "Sum",      "Count", 0],
-          ["TargetResponseTime",              "Average"   "Milliseconds", 0],
-          ["TargetTLSNegotiationErrorCount",  "Sum",      "Count", 0],
-          ["UnHealthyHostCount",              "Maximum"   "Count", 0],
+          ['ActiveConnectionCount',           'Average',  'Count',  0],
+          ['ClientTLSNegotiationErrorCount',  'Sum',      'Count',  0],
+          ['ConsumedLBCapacityUnits',         'Sum',      'Count',  0],
+          ['HealthyHostCount',                'Maximum',  'Count',  0],
+          ['HTTPCode_ELB_4XX',                'Sum',      'Count',  0],
+          ['HTTPCode_ELB_5XX',                'Sum',      'Count',  0],
+          ['HTTPCode_Target_2XX_Count',       'Sum',      'Count',  0],
+          ['HTTPCode_Target_3XX_Count',       'Sum',      'Count',  0],
+          ['HTTPCode_Target_4XX_Count',       'Sum',      'Count',  0],
+          ['HTTPCode_Target_5XX_Count',       'Sum',      'Count',  0],
+          ['NewConnectionCount',              'Sum',      'Count',  0],
+          ['ProcessedBytes',                  'Average',  'Bytes',  0],
+          ['RequestCount',                    'Sum',      'Count',  0],
+          ['RejectedConnectionCount',         'Sum',      'Count',  0],
+          ['TargetConnectionErrorCount',      'Sum',      'Count',  0],
+          ['TargetResponseTime',              'Average',  'Milliseconds',  0],
+          ['TargetTLSNegotiationErrorCount',  'Sum',      'Count',  0],
+          ['UnHealthyHostCount',              'Maximum',  'Count',  0]
         ]
       end
 
@@ -38,20 +38,18 @@ module NewRelicAWS
         load_balancers.each do |load_balancer_name|
           metric_list.each do |(metric_name, statistic, unit, default_value)|
             data_point = get_data_point(
-              :namespace     => "AWS/ApplicationELB",
-              :metric_name   => metric_name,
-              :statistic     => statistic,
-              :unit          => unit,
-              :default_value => default_value,
-              :dimension     => {
-                :name  => "LoadBalancerName",
-                :value => load_balancer_name
+              namespace: 'AWS/ApplicationELB',
+              metric_name: metric_name,
+              statistic: statistic,
+              unit: unit,
+              default_value: default_value,
+              dimension: {
+                name: 'LoadBalancer',
+                value: load_balancer_name
               }
             )
             NewRelic::PlatformLogger.debug("metric_name: #{metric_name}, statistic: #{statistic}, unit: #{unit}, response: #{data_point.inspect}")
-            unless data_point.nil?
-              data_points << data_point
-            end
+            data_points << data_point unless data_point.nil?
           end
         end
         data_points
